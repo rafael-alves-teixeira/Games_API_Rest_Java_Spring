ffa3410b84c8a1d9f825f60add758fc0382a5cea
Rafael Alves Teixeira <rafael.info.tc@gmail.com>
2023-05-11 17:02:47 -0300

    GET game by id

--- src/main/java/com/devrafael/games/services/GameService.java
+++ src/main/java/com/devrafael/games/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devrafael.games.dto.GameDTO;
 import com.devrafael.games.dto.GameMinDTO;
 import com.devrafael.games.entities.Game;
 import com.devrafael.games.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result =  gameRepository.findById(id).get();
+		return new GameDTO(result);		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();		
 //		return result.stream().map(x -> new GameMinDTO(x)).toList();
