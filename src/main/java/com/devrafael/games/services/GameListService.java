70462d06ac94cef9c30917a1efef0be0c9a5b13c
Rafael Alves Teixeira <rafael.info.tc@gmail.com>
2023-07-15 22:31:31 -0300

    Move games on list

--- src/main/java/com/devrafael/games/services/GameListService.java
+++ src/main/java/com/devrafael/games/services/GameListService.java
@@ -8,13 +8,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devrafael.games.dto.GameListDTO;
 import com.devrafael.games.entities.GameList;
+import com.devrafael.games.projections.GameMinProjection;
 import com.devrafael.games.repositories.GameListRepository;
+import com.devrafael.games.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	
 		
 	@Transactional(readOnly = true)
@@ -24,5 +30,20 @@ public class GameListService {
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
 	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
 
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 }
